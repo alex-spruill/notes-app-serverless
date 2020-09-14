0bb4602b714bc3fbbaa4b2344da4532c1876f5c9
alex-spruill <70041313+alex-spruill@users.noreply.github.com>
2020-09-14 14:51:14 -0600

    this is totally a commit

--- create.js
+++ create.js
@@ -7,10 +7,10 @@ export const main = handler(async (event, context) => {
   const params = {
     TableName: process.env.tableName,
     // 'Item' contains the attributes of the item to be created
-    // - 'userId': user identities are federated through the
+    // - 'userid': user identities are federated through the
     //             Cognito Identity Pool, we will use the identity id
     //             as the user id of the authenticated user
-    // - 'noteId': a unique uuid
+    // - 'noteid': a unique uuid
     // - 'content': parsed from request body
     // - 'attachment': parsed from request body
     // - 'createdAt': current Unix timestamp
---
+++ list.js
@@ -0,0 +1,23 @@
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  const params = {
+    TableName: process.env.tableName,
+    // 'KeyConditionExpression' defines the condition for the query
+    // - 'userid = :userid': only return items with matching 'userid'
+    //   partition key
+    // 'ExpressionAttributeValues' defines the value in the condition
+    // - ':userid': defines 'userid' to be Identity Pool identity id
+    //   of the authenticated user
+    KeyConditionExpression: "userid = :userid",
+    ExpressionAttributeValues: {
+      ":userid": event.requestContext.identity.cognitoIdentityId
+    }
+  };
+
+  const result = await dynamoDb.query(params);
+
+  // Return the matching list of items in response body
+  return result.Items;
+});
